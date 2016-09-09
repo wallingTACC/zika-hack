7506033149f6e2c061cc0a247b89bb1f25ab5668
walling <walling@tacc.utexas.edu>
2016-09-09 11:09:52 -0500

    Adding empty confige for agave

--- gene_analyze_web/agave_job/agave.py
+++ gene_analyze_web/agave_job/agave.py
@@ -3,3 +3,10 @@ Created on Sep 8, 2016
 
 @author: walling
 '''
+from agavepy.agave import Agave
+
+
+def connect():
+    my_agave = Agave(api_server='https://agave.iplantc.org', username='myusername', password='mypassword')
+    
+def push_file():
---
+++ gene_analyze_web/agave_job/agave_config.py
@@ -0,0 +1,3 @@
+USERNAME='username'
+PW='pw'
+APISERVER='apiserver'
