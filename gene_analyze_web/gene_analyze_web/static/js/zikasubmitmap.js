24ffa6171e70c3a498ffd103eeeb6df59e2ab22f
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-09-09 15:05:26 -0500

    jquery each

--- gene_analyze_web/gene_analyze_web/static/js/zikasubmitmap.js
+++ gene_analyze_web/gene_analyze_web/static/js/zikasubmitmap.js
@@ -10,16 +10,16 @@ jQuery.fn.extend({
             var usrLat = $(that).attr("data-lat");
             var usrLng = $(that).attr("data-lng");
 
-            for (var item in $('[data-item="entry"]')) {
+            $('[data-item="entry"]').each(function() {
                 var newLatLng = new google.maps.LatLng({
-                    lat: parseFloat($(item).attr('data-lat')),
-                    lng: parseFloat($(item).attr('data-lng'))
+                    lat: parseFloat($(this).attr('data-lat')),
+                    lng: parseFloat($(this).attr('data-lng'))
                 });
                 var newMarker = new google.maps.Marker({
                     map: map,
                     position: newLatLng
                 });
-            }
+            });
 
             if (usrLat && usrLng) {
                 var usrLatLng = new google.maps.LatLng({
